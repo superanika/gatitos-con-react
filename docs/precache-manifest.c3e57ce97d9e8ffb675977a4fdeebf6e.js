5da9db896f84496f406e69182c793bd3481d07d6
superanika <anamaria.romerogarcia@gmail.com>
2019-05-06 20:22:24 +0200

    initial commit

---
+++ docs/precache-manifest.c3e57ce97d9e8ffb675977a4fdeebf6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ac862df072fe3d0f685922495648e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95fe5433b9809e0a510c",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "95fe5433b9809e0a510c",
+    "url": "./static/js/main.9f1c07a3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
